7ce62b7936b7ceacf1acd0085e16d6fb9a018ded
Panciz <dav.poletti@gmail.com>
2015-11-28 08:07:06 +0100

    calculateTax test

--- sales-taxes-calc/src/test/java/org/dpoletti/sales_taxes_calc/SalesTaxesCalculator.java
+++ sales-taxes-calc/src/test/java/org/dpoletti/sales_taxes_calc/SalesTaxesCalculator.java
@@ -1,7 +1,16 @@
 package org.dpoletti.sales_taxes_calc;
 
+import org.junit.Assert;
+import org.junit.Test;
 
 public class SalesTaxesCalculator{
 	
+	
+	@Test
+	public void applyTaxes(){
+		Item noTaxItem = new Item("book",12.49d);
+		Assert.assertEquals("Error calculating taxes for "+item,item.getTaxAmount(), 12.49);
+		
+	}
 	
 }
